15bcaaa5f0fcb3315be18ad8318a5ffcbf1f651b
pagnkelly <632228198@qq.com>
2020-09-12 23:40:11 +0800

    fix

---
+++ test/fixtures/issue21.vue
@@ -0,0 +1,13 @@
+<template>
+   <div></div>
+</template>
+
+<script>
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({})
+class LintThis extends Vue { }
+
+export default LintThis
+</script>
